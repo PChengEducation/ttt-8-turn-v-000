de7d214b1c6916f0427d2f52e88382fa79bc124b
pchengeducation@hotmail.com <pchengeducation@hotmail.com>
2018-06-22 17:37:47 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,7 @@ puts "Please enter 1-9:"
 user_input = gets.strip
 index = input_to_index(user_input)
 if valid_move?(board, index)
-  board[user_input]="X"
+  board[index]="X"
   display_board(board)
 else
   turn(board)
